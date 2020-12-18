923833746a27919edf6d4404cdcce1fc28eb0f84
Anmol Sethi <hi@nhooyr.io>
2020-12-18 04:25:17 -0500

    proxy_agent: Implement $HTTPS_PROXY
    My reasoning to not was bred out of confusion.
    
    $HTTP_PROXY has to actually speak HTTP and supports both HTTP/HTTPS
    resources but is not end to end encrypted.
    
    $HTTPS_PROXY is just a TCP tunnel to the actual destination and so only
    supports HTTPS. If set, we should prefer it to $HTTP_PROXY for security.

--- src/node/proxy_agent.ts
+++ src/node/proxy_agent.ts
@@ -4,7 +4,7 @@ import * as proxyagent from "proxy-agent"
 
 /**
  * This file does not have anything to do with the code-server proxy.
- * It's for $HTTP_PROXY support!
+ * It's for $HTTP_PROXY/$HTTPS_PROXY support!
  * - https://github.com/cdr/code-server/issues/124
  * - https://www.npmjs.com/package/proxy-agent
  *
@@ -16,24 +16,36 @@ import * as proxyagent from "proxy-agent"
 
 /**
  * monkeyPatch patches the node HTTP/HTTPS library to route all requests through our
- * custom agent from the proxyAgent package.
+ * custom agents from the proxy-agent package.
+ *
+ * None of our code ever passes in a explicit agent to the http modules but VS Code's
+ * does sometimes but only when a user sets the http.proxy configuration.
+ * See https://code.visualstudio.com/docs/setup/network#_legacy-proxy-server-support
+ *
+ * Even if they do, it's probably the same proxy so we should be fine! And those are
+ * deprecated anyway.
  */
 export function monkeyPatch(vscode: boolean): void {
-  // We do not support HTTPS_PROXY here to avoid confusion. proxy-agent will automatically
-  // use the correct protocol to connect to the proxy depending on the requested URL.
-  //
-  // We could implement support ourselves to allow people to configure the proxy used for
-  // HTTPS vs HTTP but there doesn't seem to be much value in that.
-  //
-  // At least of right now, it'd just be plain confusing to support HTTPS_PROXY when proxy-agent
-  // will still use HTTP to hit it for HTTP requests.
-  const proxyURL = process.env.HTTP_PROXY || process.env.http_proxy
-  if (!proxyURL) {
-    return
+  const httpProxy = process.env.HTTP_PROXY || process.env.http_proxy
+  if (httpProxy) {
+    logger.debug(`using $HTTPS_PROXY ${httpProxy}`)
+
+    const pa = newProxyAgent(httpProxy)
+    const http = require("http")
+    http.globalAgent = pa
   }
 
-  logger.debug(`using $HTTP_PROXY ${proxyURL}`)
+  const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy
+  if (httpsProxy) {
+    logger.debug(`using $HTTP_PROXY ${httpsProxy}`)
 
+    const pa = newProxyAgent(httpsProxy)
+    const https = require("https")
+    https.globalAgent = pa
+  }
+}
+
+function newProxyAgent(proxyURL: string): http.Agent {
   let pa: http.Agent
   // The reasoning for this split is that VS Code's build process does not have
   // esModuleInterop enabled but the code-server one does. As a result depending on where
@@ -42,21 +54,9 @@ export function monkeyPatch(vscode: boolean): void {
   // I can't enable esModuleInterop in VS Code's build process as it breaks and spits out
   // a huge number of errors.
   if (vscode) {
-    pa = new (proxyagent as any)(proxyURL)
+    pa = new (proxyagent as any)(httpProxy)
   } else {
-    pa = new (proxyagent as any).default(proxyURL)
+    pa = new (proxyagent as any).default(httpProxy)
   }
-
-  /**
-   * None of our code ever passes in a explicit agent to the http modules but VS Code's
-   * does sometimes but only when a user sets the http.proxy configuration.
-   * See https://code.visualstudio.com/docs/setup/network#_legacy-proxy-server-support
-   *
-   * Even if they do, it's probably the same proxy so we should be fine! And those are
-   * deprecated anyway.
-   */
-  const http = require("http")
-  const https = require("https")
-  http.globalAgent = pa
-  https.globalAgent = pa
+  return pa
 }
